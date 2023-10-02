147c3ee03ade2597a24729e6a4c8bef081148a89
YOUBrian <brian758758@gmail.com>
2023-10-02 20:23:30 +0800

    (feat) change hobby logic, loading lottie (fix) recommend hobby bug

--- HobbyExplore/app/src/main/java/com/example/hobbyexplore/chatgpt/ChatGptFragment.kt
+++ HobbyExplore/app/src/main/java/com/example/hobbyexplore/chatgpt/ChatGptFragment.kt
@@ -3,65 +3,66 @@ package com.example.hobbyexplore.chatgpt
 import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil.setContentView
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.hobbyexplore.R
 import com.example.hobbyexplore.data.gpt.ChatGPTMessage
-import com.example.hobbyexplore.databinding.FragmentChatGptBinding
 import com.example.hobbyexplore.databinding.FragmentSystemRecommendsHobbyBinding
-
 
 class ChatGptFragment : Fragment() {
 
-    private lateinit var chatGptViewModel: ChatGptViewModel
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(ChatGptViewModel::class.java)
+    }
 
-    private lateinit var viewModel: ChatGptViewModel
-
-    @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        val binding = FragmentChatGptBinding.inflate(inflater)
         val binding = FragmentSystemRecommendsHobbyBinding.inflate(inflater)
 
+        binding.selectButton.visibility = View.GONE
+        binding.changeButton.visibility = View.GONE
+        binding.loadingAnimation.visibility = View.VISIBLE
+        binding.loadingAnimation.playAnimation()
 
-        chatGptViewModel = ViewModelProvider(this)[ChatGptViewModel::class.java]
         val typeString = arguments?.getString("typeString") ?: return null
 
-//        val llm = LinearLayoutManager(context)
-//        binding.recyclerView.layoutManager = llm
-//
-//        chatGptViewModel.messageList.observe(viewLifecycleOwner){messages ->
-//            val adapter = ChatGptAdapter(messages)
-//            binding.recyclerView.adapter = adapter
-//        }
-//
+        viewModel.introduceList.observe(viewLifecycleOwner, Observer { introduce ->
+            introduce?.let {
+                binding.loadingAnimation.pauseAnimation()
+                binding.loadingAnimation.visibility = View.GONE
+                binding.selectButton.visibility = View.VISIBLE
+                binding.changeButton.visibility = View.VISIBLE
 
-            chatGptViewModel.addToChat(typeString, ChatGPTMessage.SENT_BY_ME,chatGptViewModel.getCurrentTimestamp())
-            chatGptViewModel.callApi(typeString)
+                binding.detailTitle.text = introduce.title
+                binding.detailContent.text = introduce.content
+                Glide.with(this@ChatGptFragment)
+                    .load(introduce.image)
+                    .into(binding.detailImage)
+            }
+        })
 
+        viewModel.addToChat(typeString, ChatGPTMessage.SENT_BY_ME, viewModel.getCurrentTimestamp())
+        viewModel.callApi(typeString)
 
         binding.changeButton.setOnClickListener {
-            binding.detailImage.setImageResource(R.drawable.baseball)
-            binding.detailTitle.text = "棒球"
-            binding.detailContent.text = "為一種團體球類運動，由人數最少為9人的兩支隊伍在一個扇形的棒球場進行攻擊與守備。棒球球員分為攻方與守方，攻方球員利用球棒將守方投擲的球擊出，隨後以逆時針方向沿著四個壘位進行跑壘，當成功回到本壘就可得1分；而守方則利用手套將攻方擊出的球接住或透過傳球將攻方球員封殺或觸殺出局。比賽中，兩隊輪流攻守，比賽結束後，得分較高的一隊會勝出；若雙方得分仍相同，則進入延長賽；在延長賽中每一局規則均按照正規賽最後一個半局計算，在該局下半局結束後若分數有差距即結束比賽。  棒球起源於大英帝國本土，後流傳到北美洲；在美國成為國民運動後，分別流傳至中南美洲及亞洲東部；目前在美洲中北部及亞洲東部較為盛行；棒球曾經在1992年至2008年為奧林匹克運動會的正式項目，但之後被從正式項目中移除，自此之後並非每屆舉辦；目前棒球的主要國際賽事是世界棒球經典賽與世界棒球12強賽，在世界多個地區皆有職業棒球聯盟，而美國職棒大聯盟被公認是世界強度最高的職業棒球聯盟。"
+            binding.loadingAnimation.visibility = View.VISIBLE
+            binding.loadingAnimation.playAnimation()
+
+            val randomSport = viewModel.getRandomSportWithoutRepetition()
+            viewModel.getHobbyData(randomSport)
         }
 
         binding.selectButton.setOnClickListener {
-            it.findNavController()
-                .navigate(ChatGptFragmentDirections.actionChatGptFragmentToEnterBudgetFragment())
+            it.findNavController().navigate(ChatGptFragmentDirections.actionChatGptFragmentToEnterBudgetFragment())
         }
-
 
         return binding.root
     }
-
-
 }
--- HobbyExplore/app/src/main/java/com/example/hobbyexplore/chatgpt/ChatGptViewModel.kt
+++ HobbyExplore/app/src/main/java/com/example/hobbyexplore/chatgpt/ChatGptViewModel.kt
@@ -22,12 +22,17 @@ import java.util.Date
 import java.util.Locale
 
 class ChatGptViewModel : ViewModel() {
+    // All the sports
+    private val allSports = listOf("baseball", "basketball", "tennis", "badminton", "table_tennis", "volleyball")
+    // Copy of all sports to be modified during random selection
+    private var availableSports = mutableListOf<String>().apply { addAll(allSports) }
+
 
     val db = Firebase.firestore
 
-    private val _introduceList = MutableLiveData<List<Introduce>>()
+    private val _introduceList = MutableLiveData<Introduce?>()
 
-    val introduceList: LiveData<List<Introduce>>
+    val introduceList: LiveData<Introduce?>
         get() = _introduceList
 
     private val _messageList = MutableLiveData<MutableList<ChatGPTMessage>>()
@@ -49,49 +54,50 @@ class ChatGptViewModel : ViewModel() {
         addToChat(response,ChatGPTMessage.SENT_BY_BOT,getCurrentTimestamp())
     }
 
-    fun callApi(typeString : String){
-        addToChat("Typing....",ChatGPTMessage.SENT_BY_BOT,getCurrentTimestamp())
-
+    fun callApi(typeString: String) {
+        addToChat("Typing....", ChatGPTMessage.SENT_BY_BOT, getCurrentTimestamp())
         val completionRequest = CompletionRequest(
             model = "text-davinci-003",
             prompt = typeString,
             max_tokens = 100
         )
 
+        val sportRecommendation = getRandomSport() // Get the sport recommendation here.
+
         viewModelScope.launch {
             try {
                 val response = ApiClient.apiService.getCompletions(completionRequest)
-                handleApiResponse(response)
-            }catch (e: SocketTimeoutException){
+                handleApiResponse(response, sportRecommendation)
+            } catch (e: SocketTimeoutException) {
                 addResponse("Timeout :  $e")
             }
         }
     }
 
-//    private suspend fun handleApiResponse(response: Response<CompletionResponse>) {
-//        withContext(Dispatchers.Main){
-//            if (response.isSuccessful){
-//                response.body()?.let { completionResponse ->
-//                    val result = completionResponse.choices.firstOrNull()?.text
-//                    if (result != null){
-//                        addResponse(result.trim())
-//                    }else{
-//                        addResponse("No choices found")
-//                    }
-//                }
-//            }else{
+
+
+    //
+//    private suspend fun handleApiResponse(response: Response<CompletionResponse>, sportRecommendation: String) {
+//        withContext(Dispatchers.Main) {
+//            if (response.isSuccessful) {
+//                addResponse(sportRecommendation)
+//                // 現在，我們用sportRecommendation調用getHobbyData
+//                getHobbyData(sportRecommendation)
+//                Log.i("sportResponse", "sportResponse:$sportRecommendation")
+//                Log.i("sportResponse", "addResponse:${addResponse(sportRecommendation)}")
+//            } else {
+//                val statusCode = response.code()
+//                val errorBodyStr = response.errorBody()?.string()
+//                Log.e("API_ERROR", "StatusCode: $statusCode, ErrorBody: $errorBodyStr")
 //                addResponse("Failed to get response ${response.errorBody()}")
 //            }
 //        }
-//
 //    }
-    private suspend fun handleApiResponse(response: Response<CompletionResponse>) {
+
+    private suspend fun handleApiResponse(response: Response<CompletionResponse>, sportRecommendation: String) {
         withContext(Dispatchers.Main) {
             if (response.isSuccessful) {
-                val sportRecommendation = getRandomSport()
-                addResponse(sportRecommendation)
-                Log.i("sportResponse", "sportResponse:$sportRecommendation")
-                Log.i("sportResponse", "addResponse:${addResponse(sportRecommendation)}")
+                getHobbyData(sportRecommendation)
             } else {
                 val statusCode = response.code()
                 val errorBodyStr = response.errorBody()?.string()
@@ -100,6 +106,8 @@ class ChatGptViewModel : ViewModel() {
             }
         }
     }
+
+
 
     fun getCurrentTimestamp(): String {
         return SimpleDateFormat("hh mm a", Locale.getDefault()).format(Date())
@@ -110,37 +118,42 @@ class ChatGptViewModel : ViewModel() {
         return sports.random()
     }
 
-    private fun getCategoryData(sportName:String) {
-        val docRef = db.collection("sports")//.document(sport)
-//            .document("baseball")
+
+    fun getHobbyData(sportName:String) {
+        val docRef = db.collection("sports").document(sportName) // 使用sportName而不是硬編碼的"baseball"
         docRef.addSnapshotListener { snapshot, e ->
             if (e != null) {
                 Log.w("READ_DATA", "Listen failed.", e)
                 return@addSnapshotListener
-            } else if (snapshot != null && !snapshot.metadata.hasPendingWrites()) {
-//            Log.i("getdata", "document:${snapshot}")
+            }
+
+            if (snapshot != null && snapshot.exists() && !snapshot.metadata.hasPendingWrites()) {
                 try {
-                    val introduceData = mutableListOf<Introduce>()
-
-                    for (document in snapshot) {
-                        Log.i("getdata", "document:${document.data}")
-                        Log.i("getdata", "snapshot:$snapshot")
-                        val introduce = document.toObject(Introduce::class.java)
-                        Log.i("getdata", "introduce:$introduce")
-                        if (introduce != null) {
-                            introduceData.add(introduce)
-                        }
+                    val introduce = snapshot.toObject(Introduce::class.java)
+                    if (introduce != null) {
+                        _introduceList.postValue(introduce)
+                        Log.i("gethobbydataaaaaa", "$introduce")
+                    } else {
+                        Log.e("getdataaaaaa", "Failed to convert snapshot to Introduce")
                     }
-
-
-                    _introduceList.postValue(introduceData)
-                    Log.i("getdata", "introduceData:$introduceData")
                 } catch (e: Exception) {
                     println("error: ${e.message}")
                 }
             }
-//            _refreshStatus.value = false
+
         }
     }
+
+    fun getRandomSportWithoutRepetition(): String {
+        // If all sports have been chosen, refill the list
+        if (availableSports.isEmpty()) {
+            availableSports.addAll(allSports)
+        }
+        // Randomly select and remove a sport from the list
+        val sport = availableSports.random()
+        availableSports.remove(sport)
+        return sport
+    }
+
 
 }
